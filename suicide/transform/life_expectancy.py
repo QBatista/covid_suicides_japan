1bbe2cfbfd9832855906fde86ba4b04f15ef1371
QBatista <batista.quent@gmail.com>
2021-09-01 23:47:23 +0900

    Refactor `life_expectancy.py`

--- suicide/transform/life_expectancy.py
+++ suicide/transform/life_expectancy.py
@@ -10,6 +10,8 @@ import pandas as pd
 
 AGE_GROUPS = ['0_19', '20_29', '30_39', '40_49', '50_59', '60_69', '70_79',
               '80_99']
+MALE_SHEET_NAME = '令和元年簡易生命表（男）'
+FEMALE_SHEET_NAME = '令和元年簡易生命表（女）'
 
 
 def get_series(df):
@@ -27,22 +29,36 @@ def get_series(df):
 
 
 def life_expectancy(params, load_path, save_path):
+    """
+    Clean the life expectancy data located at `load_path` and save it
+    to `save_path` based on `params['analysis_date']`.
+
+    """
+
     # Unpack arguments
     analysis_date = params['analysis_date']
 
+    # Get full paths
     load_path = os.path.join(load_path, analysis_date, 'life_expectancy.xlsx')
     save_path = os.path.join(save_path, analysis_date, 'life_expectancy.csv')
 
-
+    # Load data
     xls = pd.read_excel(load_path, None, header=None)
-    data = {'male': get_series(xls['令和元年簡易生命表（男）']),
-            'female': get_series(xls['令和元年簡易生命表（女）'])}
+    data = {'male': get_series(xls[MALE_SHEET_NAME]),
+            'female': get_series(xls[FEMALE_SHEET_NAME])}
     df = pd.DataFrame(data)
 
+    # Initialize cleaned dataframe
     df_final = pd.DataFrame(columns=['male', 'female'], index=AGE_GROUPS)
+
+    # Compute average life expectancy for each age group
     for a in AGE_GROUPS:
         df_final.loc[a, :] = df.loc[slice(*map(int, a.split('_')))].mean()
+
+    # Rename index
     df_final.index.rename('age_group', inplace=True)
+
+    # Save
     df_final.to_csv(save_path)
 
 
